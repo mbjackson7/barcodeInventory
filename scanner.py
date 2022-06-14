d9d40ea9491825611fc38c3eefc0907f759a9f15
Morgan Jackson <morgan@midwayspark.com>
2022-06-13 22:15:58 -0500

    scanner requests data from upcdatabase

--- scanner.py
+++ scanner.py
@@ -1,5 +1,29 @@
+import os
+import requests
 from dotenv import load_dotenv
 
-load_dotenv()
 
-print(API_KEY)
+
+
+
+def get_info(UPC):
+    API_KEY = os.getenv('API_KEY')
+    requestString = "https://api.upcdatabase.org/product/" + str(UPC) + "?apikey=" + str(API_KEY)
+    response = requests.get(requestString)
+    if response.status_code == 200:
+        print('Success!')
+        return response.json()
+    else:
+        return None
+        
+
+def main():
+    load_dotenv()
+    #Test UPC, from Matzos
+    UPC = "0070227500010"
+    data = get_info(UPC)
+    if data:
+        print
+
+if __name__ == "__main__":
+    main()
